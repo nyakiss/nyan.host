4455b04132f443d6040e57bafd40c25495c5ac07
nyakiss <nyakiss@yandex.ru>
2020-09-17 19:53:31 +0700

    json edit

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ const app = express();
 app.enable('trust proxy');
 
 const data = {
-  about:   require('./data/about.json')
+  about:       require('./data/about.json'),
+  links:       require('./data/links.json')
 };
 
 app.use('/static/css', expressLess(path.resolve(__dirname, 'less')));
@@ -19,11 +20,11 @@ app.engine('.hbs', exphbs({
 }));
 app.set('view engine', '.hbs');
 app.get('/',        (req, res) => res.redirect('/about'));
-app.get('/about',   (req, res) => res.render('about',   { objects: data.about, }));
+app.get('/about',   (req, res) => res.render('about',   { about: data.about, links: data.links}));
 app.use((req, res) => res.status(404).render('404'));
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).render('500');
 });
 
-app.listen(3000, () => console.log('Listening on: http://localhost:3000/'));
+app.listen(3000, () => console.log('Ok, dude, i`m on: http://localhost:3000/'));
